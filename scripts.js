08299cffdc2cef369c9e9e7c427a54832b3fc0a7
burlesquel <batumanav@gmail.com>
2022-03-04 19:33:09 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// document.addEventListener("scroll",()=>{
+//     if(document.documentElement.scrollTop > 136){
+//         document.querySelector(".header-container").style.position = "fixed"
+//         document.querySelector(".header-container").style.width = "100%"
+//     }
+//     else{
+//         document.querySelector(".header-container").style.position = ""
+//         document.querySelector(".header-container").style.width = "80%"
+//     }
+// })
